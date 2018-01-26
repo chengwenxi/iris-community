9af6c3f1c530809b76becd579117e34041e839cf
vincent <vincent@bianjie.ai>
2018-01-26 18:12:43 +0800

    add redis with test

---
+++ models/redis/redis_test.go
@@ -0,0 +1,44 @@
+package redis
+
+import (
+	"testing"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+func TestRedis(t *testing.T) {
+	InitRedis()
+	v, err := pool.Get().Do("SET", "name", "red")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(v)
+	v, err = pool.Get().Do("EXPIRE", "name", 3) //10 seconds expired
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(v)
+	v, err = pool.Get().Do("GET", "name")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if s, err := redis.String(v, err); err == nil {
+		fmt.Println(s)
+	}
+
+	time.Sleep(3 * time.Second)
+	v, err = pool.Get().Do("GET", "name")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if s, err := redis.String(v, err); err == nil {
+		fmt.Println(s)
+	}else {
+		fmt.Println(err)
+	}
+}
