e0691b4f75f7ffa52c9b8468af01d6e74b87047e
zhangzhiqiang <745124335@aa.com>
2018-01-29 15:11:18 +0800

    添加阿里云临时账号授权api

--- rest/aliyun.go
+++ rest/aliyun.go
@@ -9,7 +9,7 @@ import (
 
 func RegisterAliyun(g *gin.RouterGroup) {
 	//获取临时授权账号(文件上传)[acs:ram::1768586477174862:role/aliyunosswriteandreadrole 角色具有oss的读写权限]
-	g.GET("/stsAuth", func(context *gin.Context) {
+	g.GET("/sts/AssumeRole", func(context *gin.Context) {
 		resp := utils.AssumeRole()
 		context.JSON(http.StatusOK, resp)
 	})
